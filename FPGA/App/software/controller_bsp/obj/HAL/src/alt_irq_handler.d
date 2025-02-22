6754d6bc56011a62d6a656d28b283c5d49cbce63
nittc-kiks <kiks.nittc@gmail.com>
2025-02-22 18:21:15 +0900

    gitignoreを反映

--- FPGA/App/software/controller_bsp/obj/HAL/src/alt_irq_handler.d
+++
@@ -1,4 +0,0 @@
-obj/HAL/src/alt_irq_handler.o: HAL/src/alt_irq_handler.c system.h \
- linker.h
-system.h:
-linker.h:
--- FPGA/App/software/controller_bsp/obj/HAL/src/alt_irq_register.d
+++
@@ -1,4 +0,0 @@
-obj/HAL/src/alt_irq_register.o: HAL/src/alt_irq_register.c system.h \
- linker.h
-system.h:
-linker.h:
